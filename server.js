f335c45de743aaca8e0ba60c2fb14f5ef5aa9649
Adarsh Jayakumar <Adarsh Jayakumar>
2017-02-26 08:20:55 -0600

    Adding https request to express

--- server.js
+++ server.js
@@ -1,20 +1,34 @@
-const express = require('express');
+const express = require('express')
+const fs = require('fs')
+const crypto = require('crypto')
 const bodyParser = require('body-parser')
+const https = require('https')
+const http = require('http')
 const app = express()
-
+app.set('view engine','ejs')
+app.use(bodyParser.urlencoded({extended:true}))
 var db
+var options = {
+    key: fs.readFileSync('/path/to/key.pem'),
+    cert: fs.readFileSync('/path/to/cert.cert')
+};
+
+https.createserver(options,function(req,res){
+    res.writeHead(200);
+    res.end('hello world\n');
+}).listen(8080);
+
 const MongoClient = require('mongodb').MongoClient
 MongoClient.connect('mongodb://adarshjaya12:Ad9940114541@ds161179.mlab.com:61179/resumingly',function(err,database){
 	if(err) return console.log(err)
 	db = database;
 	app.listen(8080,function(){
-		console.log('Listening on 8080 database')
+		console.log('Listening on 8080 )
 	})
 
 })
-app.set('view engine','ejs')
-app.use(bodyParser.urlencoded({extended:true}))
-app.listen(3000,function(){
+
+app.listen(8080,function(){
 app.get('/',function(req,res)
 	{
 
