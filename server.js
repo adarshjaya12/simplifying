ab0b77f0fe8fcd1c45e8b44c5dab1ce625008714
Adarsh Jayakumar <Adarsh Jayakumar>
2017-03-11 05:52:35 -0600

    Included EJS viewengine to the server

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
+var app = require('express');
 var fs = require('fs');
+var bodyParser = require('body-parser');
 var https = require('https');
-var app = require('express')();
 var options = {
    key  : fs.readFileSync('/root/server.key'),
    cert : fs.readFileSync('/root/adarshjayakumar_me.crt')
 };
-
+app.set('view engine','ejs')
+app.use(bodyParser.urlencoded({extended:true}))
 app.get('/', function (req, res) {
-   res.send('Hello World!');
+   res.render('index.ejs');
 });
 
 https.createServer(options, app).listen(process.env.PORT || 8000,function () {
