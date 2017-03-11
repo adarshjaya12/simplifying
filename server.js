9e1811dd73e49fe2fbb7c61618f8342f82c9e977
root <adarsh diigtal ocean>
2017-03-11 16:09:08 -0500

    Updated Server to work on port 9XXX in SSL

--- server.js
+++ server.js
@@ -1,20 +1,34 @@
-var app = require('express');
-var fs = require('fs');
-var bodyParser = require('body-parser');
-var http =  require('http');
-var https = require('https');
+var express = require('express')
+var fs = require('fs')
+var http =  require('http')
+var https = require('https')
+var app = express()
 var options = {
-   key  : fs.readFileSync('/root/server.key'),
-   cert : fs.readFileSync('/root/adarshjayakumar_me.crt')
+   key  : fs.readFileSync('/root/server.key','utf8'),
+   cert : fs.readFileSync('/root/adarshjayakumar_me.crt','utf8')
 };
-app.set('view engine','ejs')
-app.use(bodyParser.urlencoded({extended:true}))
-app.get('/', function (req, res) {
-   res.render('views/index.ejs');
-});
 
-app.get('/dummy',function(req,res){
-   res.write("Dummy Yummy");
-});
-https.createServer(options, app).listen(8000,'127.0.0.1');
-});
+app.set('views',__dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+
+https.createServer(options, app)
+http.createServer(app)
+
+app.listen(9011,function(){
+	app.get('/', function (req, res) {
+   		res.render('index');
+	});
+
+	app.get('/dummy',function(req,res){
+   		res.write("Dummy Yummy");
+	});
+})
+
+
+app.listen(5091,function(){
+	app.get('/',function(req,res){
+		res.write('From 5991');
+	});
+})
